a7e93bc1f9a2eb2daae8403b701c3c580f541d02
packer-ci <62970560+packer-ci@users.noreply.github.com>
2020-04-29 18:26:01 +0000

    cut version 0.0.6

--- website/config.rb
+++ website/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.packer.io/"
 
 activate :hashicorp do |h|
   h.name         = "packer"
-  h.version      = "0.0.5"
+  h.version      = "0.0.6"
   h.github_slug  = "hashicorp/packer"
   h.website_root = "website"
 end
